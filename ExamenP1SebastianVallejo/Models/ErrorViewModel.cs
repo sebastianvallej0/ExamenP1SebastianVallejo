e59597cb3edd727c3f6060f5265663a0fbfc8017
laboratorio <laboratorio@UPOAULA10717>
2025-04-29 07:22:34 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP1SebastianVallejo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenP1SebastianVallejo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
